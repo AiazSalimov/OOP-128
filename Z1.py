0abb97b2b626cdd6a0cccca2f8fb0dc43fc55b1e
Aiaz <ayazpskov1@gmail.com>
2025-05-09 22:31:13 +0300

    Updated Z1.py with new changes

--- Z1.py
+++ Z1.py
@@ -7,6 +7,15 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecture(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -31,15 +40,21 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
-# Пример использования:
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
 cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
 
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_lecturer = Lecturer('Agent', 'Smith')
+cool_lecturer.courses_attached += ['Python']
 
-print(best_student.grades)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 9)
+cool_reviewer.rate_hw(best_student, 'Python', 8)
+
+best_student.rate_lecture(cool_lecturer, 'Python', 10)
+best_student.rate_lecture(cool_lecturer, 'Python', 9)
+
+print(f"Оценки студента {best_student.name} по курсу Python:", best_student.grades)
+print(f"Оценки лектора {cool_lecturer.name} по курсу Python:", cool_lecturer.grades)
